cc2abf896933782ca12d5eecad956b42ed077761
Adish KATTEL <akattel@e3r9p17.42.fr>
2020-02-17 22:05:24 +0100

    c03

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akattel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/16 23:34:37 by akattel           #+#    #+#             */
-/*   Updated: 2020/02/17 16:55:46 by akattel          ###   ########.fr       */
+/*   Created: 2020/02/17 19:39:08 by akattel           #+#    #+#             */
+/*   Updated: 2020/02/17 20:04:25 by akattel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-char *ft_strcat(char *dest, char *src)
+char *ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	int i;
 	int j;
 	
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0')
+	while (dest[i] != '\0' && i<nb)
 		i++;
-	while(src[j] !='\0')
+	while(src[j] !='\0' && j<nb)
 	{
 		dest[i + j] = src [j];
 		j++;
@@ -33,10 +33,13 @@ char *ft_strcat(char *dest, char *src)
 
 int main(void)
 {
+	unsigned int nb;
+	nb = 3;
 	char dest[] = "rock";
 
 	char src[] = "star";
 
-	printf("%s",ft_strcat(dest, src));
+	printf("%s",ft_strncat(dest, src, nb));
 	return (0);
 }
+
