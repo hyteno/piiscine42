93780ccb78d801a8cb04f77cd943c455bf781394
Adish KATTEL <akattel@e3r3p20.42.fr>
2020-02-20 12:57:52 +0100

    ex02

--- c4/ex02/ft_putnbr.c
+++ c4/ex02/ft_putnbr.c
@@ -1,37 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akattel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/20 10:49:52 by akattel           #+#    #+#             */
-/*   Updated: 2020/02/20 10:58:46 by akattel          ###   ########.fr       */
+/*   Created: 2020/02/20 12:56:21 by akattel           #+#    #+#             */
+/*   Updated: 2020/02/20 12:56:27 by akattel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_putstr(char *str)
+void	ft_putnbr(int nb)
 {
-	int i;
-	i = 0;
-	while (str[i] != '\0')
+	if (nb == -2147483648)
 	{
-		ft_putchar(str[i]);
-		i++;
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * (-1));
+	}
+	else
+	{
+		if (nb >= 10)
+			ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + '0');
 	}
 }
 
 int main()
 {
-	char str[] = "helohellohelllo";
-	ft_putstr(str);
-	return (0);
+	int nb;
+	nb = 42;
+	ft_putnbr(nb);
+	return 0;
 }
